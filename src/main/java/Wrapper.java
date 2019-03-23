5baac88b7010c0584ffe0d988b69aa8822eb1227
Anna Rodriguez Gallego <anna.rodriguez@scmspain.com>
2019-03-23 13:50:16 +0100

    given_a_single_word_greather_than_line_should_return_two_lines

--- src/main/java/Wrapper.java
+++ src/main/java/Wrapper.java
@@ -1,5 +1,11 @@
 public class Wrapper {
   public static String execute(String text, int max_line_length) {
-    return text;
+    text=text.trim();
+
+    if(text.length()<=max_line_length){
+      return text;
+    }else{
+      return text.substring(0,max_line_length) + "\n" + text.substring(max_line_length);
+    }
   }
 }
--- src/test/java/WrapTest.java
+++ src/test/java/WrapTest.java
@@ -14,4 +14,16 @@ public class WrapTest {
 
     assertThat(output).isEqualTo(expected_output);
   }
+
+  @Test
+  public void given_a_single_word_greather_than_line_should_return_two_lines() {
+    String input = "expected";
+    int max_line_length = 5;
+    String expected_output = "expec\nted";
+
+    String output = Wrapper.execute(input, max_line_length);
+
+    assertThat(output).isEqualTo(expected_output);
+  }
+
 }
