d79142121bbb725182f7b3bbc5e9793b06d463c1
Anna Rodriguez Gallego <anna.rodriguez@scmspain.com>
2019-03-23 15:13:36 +0100

    Refactor

--- src/main/java/Wrapper.java
+++ src/main/java/Wrapper.java
@@ -5,21 +5,20 @@ import java.util.stream.Collectors;
 public class Wrapper {
 
   public static String execute(String text, int max_line_length) {
-    text = text.trim();
+    List<String> lines = new ArrayList();
+    toLines(text, max_line_length, lines);
+    return lines.stream().collect(Collectors.joining("\n"));
+  }
 
-    if (text.length() <= max_line_length) {
-      return text;
+  private static void toLines(String text, int max_line_length, List<String> lines) {
+    if (text.length() > max_line_length) {
+      lines.add(text.trim().substring(0, max_line_length).trim());
+      text = text.trim().substring(max_line_length);
+      toLines(text, max_line_length, lines);
     } else {
-      List<String> lines = new ArrayList();
-      while(text.length()>max_line_length){
-        lines.add(text.trim().substring(0, max_line_length).trim());
-        text = text.trim().substring(max_line_length);
-      }
-      if(!text.isEmpty()){
+      if (!text.isEmpty()) {
         lines.add(text);
       }
-
-      return lines.stream().collect(Collectors.joining("\n"));
     }
   }
 }
