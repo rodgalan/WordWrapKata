79cf1247e3580f1b9b218e048384a5d4ee3854f2
Anna Rodriguez Gallego <anna.rodriguez@scmspain.com>
2019-03-23 15:02:02 +0100

    given_three_words_with_same_length_than_line_should_return_three_lines

--- src/main/java/Wrapper.java
+++ src/main/java/Wrapper.java
@@ -1,11 +1,25 @@
-public class Wrapper {
-  public static String execute(String text, int max_line_length) {
-    text=text.trim();
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
-    if(text.length()<=max_line_length){
+public class Wrapper {
+
+  public static String execute(String text, int max_line_length) {
+    text = text.trim();
+
+    if (text.length() <= max_line_length) {
       return text;
-    }else{
-      return text.substring(0,max_line_length) + "\n" + text.substring(max_line_length);
+    } else {
+      List<String> lines = new ArrayList();
+      while(text.length()>max_line_length){
+        lines.add(text.trim().substring(0, max_line_length).trim());
+        text = text.trim().substring(max_line_length);
+      }
+      if(!text.isEmpty()){
+        lines.add(text);
+      }
+
+      return lines.stream().collect(Collectors.joining("\n"));
     }
   }
 }
--- src/test/java/WrapTest.java
+++ src/test/java/WrapTest.java
@@ -26,4 +26,26 @@ public class WrapTest {
     assertThat(output).isEqualTo(expected_output);
   }
 
+  @Test
+  public void given_a_single_word_equals_than_line_should_return_one_line() {
+    String input = "word";
+    int max_line_length = 4;
+    String expected_output= "word";
+
+    String output = Wrapper.execute(input, max_line_length);
+
+    assertThat(output).isEqualTo(expected_output);
+  }
+
+  @Test
+  public void given_three_words_with_same_length_than_line_should_return_three_lines() {
+    String input = "word1 word2 word3";
+    int max_line_length = 5;
+    String expected_output = "word1\nword2\nword3";
+
+    String output = Wrapper.execute(input, max_line_length);
+
+    assertThat(output).isEqualTo(expected_output);
+  }
+
 }
